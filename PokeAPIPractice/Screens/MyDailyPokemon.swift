bf7a9f3e73ae878428c7c2fc5bba38a4786d4df7
Mapcky <mperano625@gmail.com>
2025-02-04 10:07:06 -0300

    Daily pokemon functional

---
+++ PokeAPIPractice/Screens/MyDailyPokemon.swift
@@ -0,0 +1,18 @@
+//
+//  MyDailyPokemon.swift
+//  PokeAPIPractice
+//
+//  Created by Mateo Andres Perano on 04/02/2025.
+//
+
+import SwiftUI
+
+struct MyDailyPokemon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyDailyPokemon()
+}
